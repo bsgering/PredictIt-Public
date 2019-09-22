7868428bfcd17d25c5f9b0aa861c00ef71033218
PPM <petermar999@gmail.com>
2019-09-22 18:49:39 -0500

    This is the database file. Just start it running and it will automagically query the predictitAPI every 60 seconds and input it to a sqlite3 db whose name is the name of the month. It should change the DB every month. PPM 22Sep19 6:51 PM

---
+++ Database/Database.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[18]:
+
+
+import sqlalchemy as sql
+import database_functions as db
+import pandas as pd
+from datetime import datetime, timedelta
+import time
+
+
+# In[21]:
+
+
+def database_start():
+    while True:
+        database_name = datetime.now().strftime('%Y-%m')
+        dataframe_insert = db.contractmakerfordb()
+        engine = sql.create_engine('sqlite:///predictit_db_'+database_name+'.sqlite3', echo=False)
+        dataframe_insert.to_sql('Contracts', con=engine, if_exists='append', index_label='id')
+        engine.dispose()
+        time.sleep(60)
+
+
+# In[23]:
+
+
+database_start()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
