aa80df29701cefd5d87cee02bd7d23d6e995f1b3
eschmidt763 <52609917+eschmidt763@users.noreply.github.com>
2019-10-27 12:48:21 -0400

    Add files via upload

--- Cointergration Files/model.py
+++ Cointergration Files/model.py
@@ -4,42 +4,52 @@ Created on Sun Oct 27 10:06:50 2019
 
 @author: Erik
 """
+import statistics as stat
+import pandas as pd
+from statsmodels import coint
 
-buyaggro = .1
-sellaggro = .2
-zscore = 1.96
 
-for i in range(len(threemo)):
-    x = threemo[i]/sixmo[i]
+def pairsTradeModel(sOne, sTwo, cash, buyaggro, sellaggro, zscore):
+    ratio = []
+    value = []
+    opt1 = []
+    opt2 = []
+    sOneHold = 0
+    sTwoHold = 0
+        
+    for i in range(len(sOne)):
+        x = sOne[i]/sTwo[i]
+        
+        if i > 90:
+            mu = stat.mean(ratio)
+            sigma = stat.stdev(ratio)
+            if x > mu + (sigma*zscore):
+                cash = cash + (round(sOneHold*sellaggro)* sOne[i])
+                sOneHold -= round(sOneHold*sellaggro)
+                sTwoHold += round((cash*buyaggro)/sTwo[i])
+                cash = cash - ((round((cash*buyaggro)/sTwo[i]))* sTwo[i])
+            if x < mu - (sigma*zscore):
+                cash = cash + (round(sTwoHold*sellaggro)* sTwo[i])
+                sOneHold -= round(sTwoHold*sellaggro)
+                sTwoHold += round((cash*buyaggro)/sOne[i])
+                cash = cash - ((round((cash*buyaggro)/sOne[i]))* sOne[i])
+        
+        portfolioVal = cash + (sTwoHold*sTwo[i]) + (sOneHold*sOne[i])
+        value.append(portfolioVal)
+        
+        if i > 90:
+            opt1.append((portfolioVal/value[90])*100)
+            opt2.append((sOne[i]/sOne[90])*100)
+        ratio.append(x)
+        
+    print(coint(sOne, sTwo, autolag = 'aic'))
     
-    if i > 90:
-        mu = stat.mean(ratio)
-        sigma = stat.stdev(ratio)
-        if x > mu + (sigma*zscore):
-            cash = cash + (round(tmohold*sellaggro)* threemo[i])
-            tmohold -= round(tmohold*sellaggro)
-            smohold += round((cash*buyaggro)/sixmo[i])
-            cash = cash - ((round((cash*buyaggro)/sixmo[i]))* sixmo[i])
-        if x < mu - (sigma*zscore):
-            cash = cash + (round(smohold*sellaggro)* sixmo[i])
-            tmohold -= round(smohold*sellaggro)
-            smohold += round((cash*buyaggro)/threemo[i])
-            cash = cash - ((round((cash*buyaggro)/threemo[i]))* threemo[i])
-    
-    portfolioVal = cash + (smohold*sixmo[i]) + (tmohold*threemo[i])
-    value.append(portfolioVal)
-    
-    if i > 90:
-        opt1.append((portfolioVal/value[90])*100)
-        opt2.append((threemo[i]/threemo[90])*100)
-    ratio.append(x)
-    
-print(coint(threemo, sixmo, autolag = 'aic'))
+    cash = cash + (sOneHold * sOne[len(sOne)-1]) + (sTwoHold * sTwo[len(sTwo)-1])
+    print('Pairs Return:' + str(cash/10000))
+    print("Regular Return:" + str(sOne[len(sOne)-1]/sOne[90]))
+        
+    #plt.plot(opt1, color='green')
+    #plt.plot(opt2, color='red')
+    #plt.show()
+    return
 
-cash = cash + (tmohold * threemo[len(threemo)-1]) + (smohold * sixmo[len(sixmo)-1])
-print('Pairs Return:' + str(cash/10000))
-print("Regular Return:" + str(threemo[len(threemo)-1]/threemo[90]))
-
-#plt.plot(opt1, color='green')
-#plt.plot(opt2, color='red')
-#plt.show()
