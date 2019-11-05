1eac3fdb0c697d4d670cb271a5209af01ae798b8
PPM <petermar999@gmail.com>
2019-11-04 21:15:47 -0600

    Added a bot example, and added changes to the model notebook to try to get correct printing PPM 04-Now-19 9:16 PM

---
+++ Cointergration Files/bot.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+import discord as ds
+from discord import Webhook, RequestsWebhookAdapter
+import requests
+
+
+# In[11]:
+
+
+webhook = Webhook.partial(641106641973936133, '6j90ECRzIXIVbIMf7GDys4NEyEKaLI0vlX1hGNZo_akQBVsPjylAiiLyGy7eoq-38IJs', 
+                          adapter=RequestsWebhookAdapter())
+webhook.send('I am bot')
+
+
+# In[5]:
+
+
+https://discordapp.com/channels/641096818305794068/641096818305794070
+
+
+# In[7]:
+
+
+
+https://discordapp.com/api/webhooks/641106641973936133/
+    6j90ECRzIXIVbIMf7GDys4NEyEKaLI0vlX1hGNZo_akQBVsPjylAiiLyGy7eoq-38IJs
+
+
+# In[ ]:
+
+
+
+
