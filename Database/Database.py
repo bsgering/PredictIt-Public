2f28cf86b9f5f1a1be813fba4817481b841c6119
PPM <petermar999@gmail.com>
2019-11-09 14:13:51 -0600

    Changed db to postgres, initital test 1 PPM

--- Database/Database.py
+++ Database/Database.py
@@ -9,16 +9,13 @@ import database_functions as db
 import pandas as pd
 from datetime import datetime, timedelta
 import time
-
-
-# In[21]:
-
+import pyscopg2
 
 def database_start():
     while True:
         database_name = datetime.now().strftime('%Y-%W')
         dataframe_insert = db.contractmakerfordb()
-        engine = sql.create_engine('sqlite:///predictit_db_'+database_name+'.sqlite3', echo=False)
+        engine = sql.create_engine('postgresql://ppm@localhost/predict_it')
         dataframe_insert.to_sql('Contracts', con=engine, if_exists='append', index_label='id')
         engine.dispose()
         time.sleep(60)
