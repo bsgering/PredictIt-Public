b3c29e4066041185b772ac33afa069f1330dc8be
PPM <petermar999@gmail.com>
2019-11-09 14:45:02 -0600

    Changed engine connection so as to use user id stuff from a connection file

--- Database/Database.py
+++ Database/Database.py
@@ -9,12 +9,14 @@ import database_functions as db
 import pandas as pd
 from datetime import datetime, timedelta
 import time
+import dbconfig.py
+
 
 def database_start():
     while True:
         database_name = datetime.now().strftime('%Y-%W')
         dataframe_insert = db.contractmakerfordb()
-        engine = sql.create_engine('postgresql://ppm:predict@localhost/predict_it')
+        engine = sql.create_engine(DATABASE_NAME)
         dataframe_insert.to_sql('Contracts', con=engine, if_exists='append', index_label='id')
         engine.dispose()
         time.sleep(60)
